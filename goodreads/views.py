68b1e0865e65d9c8860ba040b2a44c2be9478af3
Abdulkhay_Payziev <abdulkhay.payziev@gmail.com>
2022-06-14 16:21:47 +0500

    new features added

--- goodreads/views.py
+++ goodreads/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from books.models import BookReview
+
 # Create your views here.
 
 
 def landing_page(request):
     return render(request, 'landing_page.html')
+
+
+def home_page(request):
+    book_reviews = BookReview.objects.all().order_by('-created_at')
+    return render(request, 'home_page.html', {'book_reviews': book_reviews})
